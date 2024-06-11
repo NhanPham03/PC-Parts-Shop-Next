85b7ab9c27f00e3ff86112f1ba3a26e8e955861d
NhanPham03 <ph.nhan03@gmail.com>
2024-06-11 14:26:13 +0700

    Update actions

--- src/lib/actions/catalog.actions.ts
+++ src/lib/actions/catalog.actions.ts
@@ -1,35 +1,57 @@
-export async function getProducts() {
+export async function getProductsList() {
   try {
     const res = await fetch(`${process.env.API_URL}/api/products/`, {
       method: "GET",
     });
 
     if (!res.ok) {
-      throw new Error("Error getting products.");
+      throw new Error("Error getting products list.");
     }
 
-    const products = await res.json();
-    return products;
+    const productsList = await res.json();
+    return productsList;
   } catch (error) {
-    console.error("Error getting products:", error);
+    console.error("[PRODUCTS LIST]", error);
     throw error;
   }
 }
 
-export async function searchProducts({ q, type }: { q?: string, type?: string }) {
+export async function getProduct(id: number) {
   try {
-    const res = await fetch(`${process.env.API_URL}/api/products/search/${q ? q : type}`, {
+    if (typeof id !== "number" || isNaN(id)) {
+      throw new Error("Invalid URL parameter.");
+    }
+
+    const res = await fetch(`${process.env.API_URL}/api/products/${encodeURIComponent(id)}/`, {
       method: "GET",
     });
 
     if (!res.ok) {
+      throw new Error(`Error getting product #${encodeURIComponent(id)}`);
+    }
+
+    const product = await res.json();
+    return product;
+  } catch (error) {
+    console.error("[PRODUCT GET]", error);
+    throw error;
+  }
+}
+
+export async function searchProducts(query: string) {
+  try {
+    const res = await fetch(`${process.env.API_URL}/api/products/search/${encodeURIComponent(query)}/`, {
+      method: "GET",
+    });
+    
+    if (!res.ok) {
       throw new Error("Error searching products.");
     }
 
-    const products = await res.json();
-    return products;
+    const results = await res.json();
+    return results;
   } catch (error) {
-    console.error("Error searching products:", error);
+    console.error("[PRODUCTS SEARCH]", error);
     throw error;
   } 
 }
---
+++ src/lib/actions/user.actions.ts
@@ -0,0 +1,115 @@
+import { LoginSchema } from "../schemas/auth.schema";
+import { RegisterSchema } from "../schemas/auth.schema";
+import { UserSchema } from "../schemas/database.schema";
+
+export async function login(data: LoginSchema) {
+  try {
+    const res = await fetch(`${process.env.API_URL}/api/login/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!res.ok) {
+      throw new Error("Login failed. Please try again later.");
+    }
+
+    const tokens = await res.json();
+    return tokens;
+  } catch (error) {
+    console.error("[LOGIN]", error);
+    throw error;
+  }
+}
+
+export async function register(data: RegisterSchema) {
+  try {
+    const res = await fetch(`${process.env.API_URL}/api/users/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!res.ok) {
+      throw new Error("Register failed. Please try again later.")
+    };
+
+    const statusCode = res.status;
+    return statusCode;
+  } catch (error) {
+    console.error("[REGISTER]", error);
+    throw error;
+  }
+}
+
+export async function getUser(id: number) {
+  try {
+    if (typeof id !== "number" || isNaN(id)) {
+      throw new Error("Invalid URL parameter.");
+    }
+
+    const res = await fetch(`${process.env.API_URL}/api/users/${encodeURIComponent(id)}/`, {
+      method: 'GET',
+    });
+
+    if (!res.ok) {
+      throw new Error(`Error getting user #${encodeURIComponent(id)}`);
+    }
+
+    const user = await res.json();
+    return user;
+  } catch (error) {
+    console.error("[USER GET]", error);
+    throw error;
+  }
+}
+
+export async function updateUser(id: number, data: UserSchema) {
+  try {
+    if (typeof id !== "number" || isNaN(id)) {
+      throw new Error("Invalid URL parameter.");
+    }
+
+    const res = await fetch(`${process.env.API_URL}/api/users/${encodeURIComponent(id)}/`, {
+      method: 'PUT',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!res.ok) {
+      throw new Error(`Error updating user #${encodeURIComponent(id)}`);
+    }
+
+    const user = await res.json();
+    return user;
+  } catch (error) {
+    console.error("[USER UPDATE]", error);
+    throw error;
+  }
+}
+
+export async function deleteUser(id: number) {
+  try {
+    if (typeof id !== "number" || isNaN(id)) {
+      throw new Error("Invalid URL parameter.");
+    }
+
+    const res = await fetch(`${process.env.API_URL}/api/users/${encodeURIComponent(id)}/`, {
+      method: 'DELETE',
+    });
+
+    if (!res.ok) {
+      throw new Error(`Error deleting user #${encodeURIComponent(id)}`);
+    }
+    return "User deleted successfully!";
+  } catch (error) {
+    console.error("[USER DELETE]", error);
+    throw error;
+  }
+}
